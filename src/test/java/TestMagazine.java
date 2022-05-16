a3b332f7ada2c633cc57fc9a13a74be1b0487506
IllyaMaz <maznichenkoo16@gmail.com>
2022-05-16 21:07:08 +0300

    debug

--- src/test/java/TestMagazine.java
+++ src/test/java/TestMagazine.java
@@ -2,14 +2,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
-
 import java.util.stream.Stream;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-
-//please always delete unused imports
 public class TestMagazine {
 
     Magezin magezin = new Magezin();
